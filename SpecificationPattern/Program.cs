bfdee6a15c422a4524e614b21bd32096135868f6
xuzz <xuzz@chinasap.cn>
2015-09-22 09:29:28 +0800

    first push Signed-off-by: xuzz <xuzz@chinasap.cn>

---
+++ SpecificationPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecificationPattern
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+
+            ISpecification<int> eqSpec = new Specification<int>( x => x > 10 ).And<int>( new Specification<int>( y => y < 20 ) ).And<int>( new Specification<int>( z => z % 5 == 0 ) );
+
+            Console.WriteLine( eqSpec.IsSatisfied( 14 ) );
+            Console.WriteLine( eqSpec.IsSatisfied( 15 ) );
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ SpecificationPattern/Specification1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecificationPattern
+{
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied( T obj );
+    }
+
+    public class Specification<T> : ISpecification<T>
+    {
+        Func<T, bool> _satisfiedby;
+        public Specification( Func<T,bool> satisfiedby )
+        { 
+            _satisfiedby = satisfiedby;
+            
+        }
+    
+        public bool  IsSatisfied(T obj)
+        {
+            return _satisfiedby( obj );
+        }
+    }
+
+    public static class SpecificationExtention
+    {
+        public static ISpecification<T> And<T>( this ISpecification<T> left,ISpecification<T> right )
+        {
+            return new Specification<T>( x => left.IsSatisfied( x ) && right.IsSatisfied( x ) );
+        }
+
+        public static ISpecification<T> Or<T>( this ISpecification<T> left, ISpecification<T> right )
+        {
+            return new Specification<T>( x => left.IsSatisfied( x ) || right.IsSatisfied( x ) );
+        }
+
+        public static ISpecification<T> NOt<T>( this ISpecification<T> left )
+        {
+            return new Specification<T>( x => !left.IsSatisfied( x )  );
+        }
+    }
+}
